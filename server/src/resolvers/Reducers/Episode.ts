1c90a3e501256f822add7124b5c6e7fbcde795bc
Dexter <dextertanyj@gmail.com>
2020-07-23 14:50:45 +0800

    Fix linting warnings in resolvers

--- server/src/resolvers/Reducers/Episode.ts
+++ server/src/resolvers/Reducers/Episode.ts
@@ -1,8 +1,17 @@
-import { Episode as EpisodeType } from "@prisma/client";
-import { Context } from "../../utils";
+import {
+  AlternativeTitle,
+  Episode as EpisodeType,
+  File,
+  Series,
+} from '@prisma/client';
+import { Context } from '../../utils';
 
 export const Episode = {
-  async alternativeTitles(parent: EpisodeType, _args: any, ctx: Context) {
+  async alternativeTitles(
+    parent: EpisodeType,
+    _args: unknown,
+    ctx: Context
+  ): Promise<AlternativeTitle[]> {
     return ctx.prisma.alternativeTitle.findMany({
       where: {
         episodeId: parent.id,
@@ -10,13 +19,21 @@ export const Episode = {
     });
   },
 
-  async series(parent: EpisodeType, _args: any, ctx: Context) {
+  async series(
+    parent: EpisodeType,
+    _args: unknown,
+    ctx: Context
+  ): Promise<Series | null> {
     return ctx.prisma.series.findOne({
       where: { id: parent.seriesId },
     });
   },
 
-  async files(parent: EpisodeType, _args: any, ctx: Context) {
+  async files(
+    parent: EpisodeType,
+    _args: unknown,
+    ctx: Context
+  ): Promise<File[]> {
     return ctx.prisma.episode
       .findOne({
         where: { id: parent.id },
