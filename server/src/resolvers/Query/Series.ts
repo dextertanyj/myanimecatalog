d455c8937c7d4b211e58f8931b6fed15f4afc123
Dexter <dextertanyj@gmail.com>
2020-07-23 14:51:37 +0800

    Fix linting errors for query resolvers

--- server/src/resolvers/Query/Series.ts
+++ server/src/resolvers/Query/Series.ts
@@ -1,12 +1,20 @@
-import { FindOneSeriesArgs, Series as SeriesType } from "@prisma/client";
-import { Context } from "../../utils";
+import { FindOneSeriesArgs, Series as SeriesType } from '@prisma/client';
+import { Context } from '../../utils';
 
 export const Series = {
-  async series(_parent: unknown, args: FindOneSeriesArgs, ctx: Context): Promise<SeriesType | null> {
+  async series(
+    _parent: unknown,
+    args: FindOneSeriesArgs,
+    ctx: Context
+  ): Promise<SeriesType | null> {
     return await ctx.prisma.series.findOne(args);
   },
 
-  async allSeries(_parent: unknown, _args: unknown, ctx: Context): Promise<SeriesType[]> {
+  async allSeries(
+    _parent: unknown,
+    _args: unknown,
+    ctx: Context
+  ): Promise<SeriesType[]> {
     return await ctx.prisma.series.findMany();
   },
-}
+};
