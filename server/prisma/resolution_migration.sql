d49e07c9e892ca9177b65a0fec53ec6d2f25de86
Dexter Tan <dev@dextertanyj.com>
2021-07-08 15:55:27 +0800

    Fix migration script

--- server/prisma/resolution_migration.sql
+++ server/prisma/resolution_migration.sql
@@ -1,13 +1,13 @@
 START TRANSACTION; 
 
-ALTER TABLE `File` ADD `resolution_height` int;
-ALTER TABLE `File` ADD `resolution_width` int;
+ALTER TABLE `File` ADD `resolutionWidth` INTEGER;
+ALTER TABLE `File` ADD `resolutionHeight` INTEGER;
 
-UPDATE `File` SET `resolutionHeight` = SUBSTRING_INDEX(`resolution`, ' × ', 1);
-UPDATE `File` SET `resolutionWidth` = SUBSTRING_INDEX(`resolution`, ' × ', -1);
+UPDATE `File` SET `resolutionWidth` = CONVERT(SUBSTRING_INDEX(`resolution`, ' × ', -1), SIGNED INTEGER);
+UPDATE `File` SET `resolutionHeight` = CONVERT(SUBSTRING_INDEX(`resolution`, ' × ', 1), SIGNED INTEGER);
 
-ALTER TABLE `File` MODIFY `resolutionHeight` int NOT NULL;
-ALTER TABLE `File` MODIFY `resolutionWidth` int NOT NULL;
+ALTER TABLE `File` MODIFY `resolutionWidth` INTEGER NOT NULL;
+ALTER TABLE `File` MODIFY `resolutionHeight` INTEGER NOT NULL;
 
 ALTER TABLE `File` DROP COLUMN `resolution`;
 
